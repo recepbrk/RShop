447271ed0c7f7ec9a95f97e5f7a7e8386f8f25df
Recep Güzel <recepgzelr@gmail.com>
2024-01-04 22:41:11 +0300

    first commit <3

---
+++ app/src/main/java/com/example/rshop/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.rshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.rshop.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController:NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        navController=Navigation.findNavController(this,R.id.nav_host_fragment_container)
+        setupWithNavController(binding.bottomNavigationView,navController)
+    }
+}
